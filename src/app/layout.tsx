1ff95f658e7865f4764e546f945d6de8d8d68232
henron1 <40742091+henron1@users.noreply.github.com>
2024-06-21 14:19:58 -0400

    main: header,footer, layout, container, avatar and date formatter utils for future use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,25 @@
+import { type Metadata } from 'next'
+
 import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
 
 import '@/styles/tailwind.css'
+
+// TODO: add actual description later
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Henry Neal',
+    default: 'Henry Neal - Software Engineer, Musician, and amateur mycologist',
+  },
+  description:
+    'I’m Henry, a software designer and entrepreneur based in Asheville. I’m the founder and CEO of Golden Tear Origin, where we .....',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
 
 export default function RootLayout({
   children,
@@ -11,7 +30,9 @@ export default function RootLayout({
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
-          <div className="flex w-full"></div>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
         </Providers>
       </body>
     </html>
